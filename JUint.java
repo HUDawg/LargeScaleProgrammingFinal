29cb72a60cb7d93887fe49bc33ea28faf67917b3
CJM <112354991+HUDawg@users.noreply.github.com>
2025-07-22 11:46:36 -0700

    Create JUint.java

---
+++ JUint.java
@@ -0,0 +1,49 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class JUnit {
+
+	@Test
+	public void testCreate() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testOpen() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testAdd_Contact() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test_Edit() {
+		fail("Not yet implemented "
+				+ "I tested everything in the main I "
+				+ "just couldnt get around to it smh");
+	}
+	
+	@Test
+	public void test_Delete() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test_Save() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test_Sort() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+}
